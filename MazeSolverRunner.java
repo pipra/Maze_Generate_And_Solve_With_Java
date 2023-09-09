1e2a1c35fb0e7a3737c55f896aacbcb782afe634
Habibur Rahman <habibur.191522@gmail.com>
2023-09-09 17:09:44 +0600

    MazeSolverRunner tamplet is complete

--- MazeSolverRunner.java
+++ MazeSolverRunner.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 class MazeSolverRunner{
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Welcome to the Maze Solver!");
         
+        while (true) {
+            System.out.println("Menu:");
+            System.out.println("1. Generate Maze");
+            System.out.println("2. Solve Maze");
+            System.out.println("3. Exit");
+
+            System.out.print("Please select an option: ");
+            int response = in.nextInt();
+
+            switch (response) {
+                
+            }
+
+            System.out.println();
+        }
     }
 }
